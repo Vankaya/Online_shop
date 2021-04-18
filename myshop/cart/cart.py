71225062cd7213108934eb47ab64a38eebd32273
vankaya <van4ka7@gmailcom>
2021-04-18 04:18:36 +0300

    second

--- myshop/cart/cart.py
+++ myshop/cart/cart.py
@@ -7,7 +7,7 @@ class Cart(object):
 
     def __init__(self, request):
         """
-        Initialize the cart
+        Initialize the cart.
         """
         self.session = request.session
         cart = self.session.get(settings.CART_SESSION_ID)
@@ -16,40 +16,14 @@ class Cart(object):
             cart = self.session[settings.CART_SESSION_ID] = {}
         self.cart = cart
 
-    def add(self, product, quantity=1, update_quantity=False):
-        """
-        Add a product to the cart or update its quantity
-        """
-        product_id = str(product.id)
-        if product_id not in self.cart:
-            self.cart[product_id] = {'quantity': 0, 'price': str(product.price)}
-        if update_quantity:
-            self.cart[product_id]['quantity'] = quantity
-        else:
-            self.cart[product_id]['quantity'] += quantity
-        self.save()
-
-    def save(self):
-        # mark the session as "modified" to make sure it gets saved sel.session.modified = True
-        self.session.modified = True
-
-    def remove(self, product):
-        """
-        Remove a product from the cart
-        """
-        product_id = str(product.id)
-        if product_id in self.cart:
-            del self.cart[product_id]
-            self.save()
-
     def __iter__(self):
         """
-        iterate over the items in the cart and get the products
-        from database
+        Iterate over the items in the cart and get the products
+        from the database.
         """
-        products_ids = self.cart.keys()
-        # get the product objects snd add them to the cart
-        products = Product.objects.filter(id__in=products_ids)
+        product_ids = self.cart.keys()
+        # get the product objects and add them to the cart
+        products = Product.objects.filter(id__in=product_ids)
 
         cart = self.cart.copy()
         for product in products:
@@ -62,9 +36,36 @@ class Cart(object):
 
     def __len__(self):
         """
-        Count all items in cart
+        Count all items in the cart.
         """
         return sum(item['quantity'] for item in self.cart.values())
+
+    def add(self, product, quantity=1, update_quantity=False):
+        """
+        Add a product to the cart or update its quantity.
+        """
+        product_id = str(product.id)
+        if product_id not in self.cart:
+            self.cart[product_id] = {'quantity': 0,
+                                     'price': str(product.price)}
+        if update_quantity:
+            self.cart[product_id]['quantity'] = quantity
+        else:
+            self.cart[product_id]['quantity'] += quantity
+        self.save()
+
+    def save(self):
+        # mark the session as "modified" to make sure it gets saved
+        self.session.modified = True
+
+    def remove(self, product):
+        """
+        Remove a product from the cart.
+        """
+        product_id = str(product.id)
+        if product_id in self.cart:
+            del self.cart[product_id]
+            self.save()
 
     def get_total_price(self):
         return sum(Decimal(item['price']) * item['quantity'] for item in self.cart.values())
